2b9829a5bc5ff50bb7c6fae6e801e999a121d12f
damianmore <dmore0126@gmail.com>
2025-11-08 18:56:22 -0800

    removed image domain from config

--- portfolio-app/next.config.ts
+++ portfolio-app/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ["via.placeholder.com"], // whitelist this domain
-  }
+
 };
 
 
