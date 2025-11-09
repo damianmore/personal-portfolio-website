4f4b161212532c5e2e3157bfdc1d839c02831258
damianmore <dmore0126@gmail.com>
2025-11-08 16:50:38 -0800

    initial react commit with all resource files I currenltly plan to use

---
+++ portfolio-app/next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ["via.placeholder.com"], // whitelist this domain
+  }
+};
+
+
+
+export default nextConfig;
