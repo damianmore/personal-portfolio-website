d46a11d1136666d7bbfe4735de8a60c9d14ea845
damianmore <dmore0126@gmail.com>
2025-11-08 16:51:31 -0800

    planned contact page

---
+++ portfolio-app/app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function ContactPage() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-semibold">About Page</h1>
+      <p className="text-zinc-600 dark:text-zinc-400 mt-4">
+        This is the About page sin Next.js!
+      </p>
+    </main>
+  )
+};
